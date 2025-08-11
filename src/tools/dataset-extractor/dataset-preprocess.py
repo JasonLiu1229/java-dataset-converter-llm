c5729922c3645462ee3fceb14539edf854a80fdf
Jason Liu <Liujason2003@gmail.com>
2025-08-11 13:14:51 +0200

    add progress bar for deletion

--- src/tools/dataset-extractor/dataset-preprocess.py
+++ src/tools/dataset-extractor/dataset-preprocess.py
@@ -23,15 +23,19 @@ def extract_methods_from_dataset(tests_file, dir_out, size="l"):
             print(
                 "Existing files in the output directory found, deleting files before extracting methods"
             )
-        for filename in os.listdir(dir_out):
-            file_path = os.path.join(dir_out, filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print("Failed to delete %s. Reason: %s" % (file_path, e))
+        with tqdm.tqdm(
+            total=len(os.listdir(dir_out)), desc="Deleting existing files"
+        ) as dbar:
+            for filename in os.listdir(dir_out):
+                file_path = os.path.join(dir_out, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print("Failed to delete %s. Reason: %s" % (file_path, e))
+                dbar.update(1)
 
     current_size = LARGE
 
